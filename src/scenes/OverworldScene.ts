e58f188626209c13910acd2e4f02b00c3ac6a138
Omar Ali Khan <oakhan3@gmail.com>
2026-03-19 19:01:26 -0400

    chore: Update overworld placeholder text.

--- src/scenes/OverworldScene.ts
+++ src/scenes/OverworldScene.ts
@@ -9,7 +9,7 @@ export class OverworldScene extends Phaser.Scene {
     this.cameras.main.setBackgroundColor('#4a8c5c')
 
     this.add
-      .text(this.cameras.main.centerX, this.cameras.main.centerY, 'OVERWORLD', {
+      .text(this.cameras.main.centerX, this.cameras.main.centerY, 'Coming Soon!!!', {
         fontFamily: '"Press Start 2P"',
         fontSize: '8px',
         color: '#fff',
